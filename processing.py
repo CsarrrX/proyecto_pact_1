58fa6af50af5194da097ec1449abd33a6b3f69ff
csarrrx <cesarpeam@proton.me>
2026-03-01 15:55:24 -0600

    changes

--- processing.py
+++ processing.py
@@ -20,13 +20,16 @@ print("\n")
 # Cambiamos los porcentajes por decimales usables
 dataset["Uninsured Rate (2010)"] = dataset["Uninsured Rate (2010)"].str.replace("%", "").astype(float) / 100
 dataset["Uninsured Rate (2015)"] = dataset["Uninsured Rate (2015)"].str.replace("%", "").astype(float) / 100
-dataset["Uninsured Rate Change (2015)"] = dataset["Uninsured Rate Change (2015)"].str.replace("%", "").astype(float) / 100
+dataset["Uninsured Rate Change (2010-2015)"] = dataset["Uninsured Rate Change (2010-2015)"].str.replace("%", "").astype(float) / 100
 
 # Cambiamos las cantidades de dolares a enteros usables
 dataset["Average Monthly Tax Credit (2016)"] = dataset["Average Monthly Tax Credit (2016)"].str.replace("$", "").astype(int)
 
 # Cambiamos las <<objetos>> de la expansión por booleanos
-dataset["State Medicaid Expansion (2016)"] 
+dataset["State Medicaid Expansion (2016)"] = dataset["State Medicaid Expansion (2016)"].astype(str)
+dataset["State Medicaid Expansion (2016)"] = dataset["State Medicaid Expansion (2016)"].map({"True" : True, "False" : False}) 
+
+print(dataset.info())
 
 
 
